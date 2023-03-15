dbe838cb54437d2196c7b6e262aca4c820967cbf
Taimoor Ali <taimoorali083@gmail.com>
2023-03-16 00:24:02 +0500

    25 video of react app

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
   render() {
-    let{title,description}= this.props;
+    let{title,description,imgUrl}= this.props;
     return (
       <div className='py-3'>
         <div className="card " style={{width:"18rem"}}>
-          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..."/>
+          <img src={imgUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text"> {description}</p>
-            <a href="/" className="btn btn-primary">Read More</a>
+            <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>
