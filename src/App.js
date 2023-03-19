bfee77aa4084ec7401d083c8e5a3d97ec24878fb
Taimoor Ali <taimoorali083@gmail.com>
2023-03-19 09:34:20 +0500

    working app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ export default class App extends Component {
            <BrowserRouter> 
         <NavBar/>
         <Routes>
+        <Route path="/" element={<News pageSize={5} country="in" category="general"/>} />
+
         <Route path="/science" element={<News pageSize={5} country="in" category="science"/>} />
+        <Route path="/business" element={<News pageSize={5} country="in" category="business"/>} />
+        <Route path="/entertainment" element={<News pageSize={5} country="in" category="entertainment"/>} />
+        <Route path="/general" element={<News pageSize={5} country="in" category="general"/>} />
+        <Route path="/health" element={<News pageSize={5} country="in" category="health"/>} />
+        <Route path="/sports" element={<News pageSize={5} country="in" category="sports"/>} />
+        <Route path="/technology" element={<News pageSize={5} country="in" category="technology"/>} />
         
         </Routes>
         </BrowserRouter>
